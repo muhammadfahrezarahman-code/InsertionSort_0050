a21910c082c7c071b66695eb63a7a027e41b268b
muhammadfahrezarahman-code <anwarfahreza4@gmail.com>
2026-03-12 15:31:09 +0700

    menambah cout dan for ke void input

--- insertionsort.cpp
+++ insertionsort.cpp
@@ -4,3 +4,30 @@ using namespace std;
 int arr[20];
 int n;
 
+void input()
+{
+    while (true)
+    {
+        cout << "Masukan Jumlah Data Pada Array : ";
+        cin >> n;
+
+        if (n <= 20)
+        {
+            break;
+        }
+        else
+        {
+            cout << "\nArray Tidak Boleh Lebih Dari 20\n";
+        }
+    }
+    cout << endl;
+    cout << "=============================================";
+    cout << "==========Masukan Elemen Array==============";
+    cout << "=============================================" << endl;
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << "Data ke-" << (i + 1) << ": ";
+        cin >> arr[i];
+    }
+}
